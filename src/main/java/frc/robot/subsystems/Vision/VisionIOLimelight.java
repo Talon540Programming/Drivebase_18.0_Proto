811478fa375571d7b95825b2bfbbe70405ffc95e
DrollerBubble59 <discordude89@gmail.com>
2025-12-18 22:55:41 -0500

    adding limelight 4 integration

--- src/main/java/frc/robot/subsystems/Vision/VisionIOLimelight.java
+++ src/main/java/frc/robot/subsystems/Vision/VisionIOLimelight.java
@@ -6,48 +6,53 @@ import frc.robot.Util.LimelightHelpers;
 import frc.robot.subsystems.Drive.CommandSwerveDrivetrain;
 
 public class VisionIOLimelight implements VisionIO {
-    private final String limelightName = Constants.kLimelightName;
+    private final String limelightOne = Constants.kLimelightOne;
+    private final String limelightTwo = Constants.kLimelightTwo;
 
     public VisionIOLimelight(){
         //sets mode for Metatag 2 and mode 0 uses robot gyro for orentation 
-        LimelightHelpers.SetIMUMode(limelightName, 0);
+        LimelightHelpers.SetIMUMode(limelightOne, 0);
+        LimelightHelpers.SetIMUMode(limelightTwo, 0);
+
     }
 
-    @Override
-    public void updateVisionIOInputs(VisionIOInputs input){
-         input.hasTarget = LimelightHelpers.getTV(limelightName);
-         input.limelightTX = LimelightHelpers.getTX(limelightName);
-         input.limelightTY = LimelightHelpers.getTY(limelightName);
-         input.limelightTA = LimelightHelpers.getTA(limelightName);
-
+    public void updateVisionIOInputs(VisionIOInputs inputOne, VisionIOInputs inputTwo) {
+        updateSingleCamera(limelightOne, inputOne);
+        updateSingleCamera(limelightTwo, inputTwo);
+    }
+    
+    private void updateSingleCamera(String cameraName, VisionIOInputs input) {
+        input.cameraName = cameraName;
+        input.hasTarget = LimelightHelpers.getTV(cameraName);
+        input.limelightTX = LimelightHelpers.getTX(cameraName);
+        input.limelightTY = LimelightHelpers.getTY(cameraName);
+        input.limelightTA = LimelightHelpers.getTA(cameraName);
+    
         var alliance = DriverStation.getAlliance();
-        if(!alliance.isEmpty() && alliance.get() == DriverStation.Alliance.Red){
+        if (!alliance.isEmpty() && alliance.get() == DriverStation.Alliance.Red) {
             input.isRedAlliance = true;
-        }
-        else if(!alliance.isEmpty() && alliance.get() == DriverStation.Alliance.Blue){
+        } else if (!alliance.isEmpty() && alliance.get() == DriverStation.Alliance.Blue) {
             input.isRedAlliance = false;
         }
-
-        LimelightHelpers.PoseEstimate metaTag2Pose = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2(limelightName);
-
-        if(metaTag2Pose != null){
+    
+        LimelightHelpers.PoseEstimate metaTag2Pose = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2(cameraName);
+    
+        if (metaTag2Pose != null) {
             input.pose = metaTag2Pose.pose;
             input.limelightTimestamp = metaTag2Pose.timestampSeconds;
             input.seenTagCount = metaTag2Pose.tagCount;
             input.avgTagDistance = metaTag2Pose.avgTagDist;
-        }
-        else{
+        } else {
             input.seenTagCount = 0;
         }
     }
+    
 
     @Override
-    public void updateLimelightYaw(CommandSwerveDrivetrain drivetrain) {
-    LimelightHelpers.SetRobotOrientation(
-        limelightName,
-        drivetrain.getPose().getRotation().getDegrees(),
-        0, 0, 0, 0, 0
-    );
-    }
+public void updateLimelightYaw(CommandSwerveDrivetrain drivetrain) {
+    double yaw = drivetrain.getPose().getRotation().getDegrees();
+    LimelightHelpers.SetRobotOrientation(limelightOne, yaw, 0, 0, 0, 0, 0);
+    LimelightHelpers.SetRobotOrientation(limelightTwo, yaw, 0, 0, 0, 0, 0);
+}
 
 }
